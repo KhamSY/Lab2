16486258c3755f713adb9d50a4aec767f2c24f91
KhamSY <KhamSY.25@ichat.sp.edu.sg>
2026-05-05 01:24:05 -0700

    lab2 mod for 3

--- bmi.py
+++ bmi.py
@@ -5,8 +5,11 @@ def calculate_bmi(height, weight):
     print("BMI:",bmi)
     if bmi<18.5:
         print ("Under Weight")
+        return -1
     elif bmi>25.0:
-        print ("Over Weight") 
+        print ("Over Weight")
+        return 1 
     else:
         print ("Normal Weight")
+        return 0
 calculate_bmi(weight=57, height=1.73)
